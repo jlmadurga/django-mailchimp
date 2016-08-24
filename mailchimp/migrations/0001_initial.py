32ed780d6c486490a7943d534cf98e1bd05ed988
Juan <juan.madurga@reclamador.es>
2016-08-24 11:34:35 +0200

    migrations for django1.7

--- mailchimp/migrations/0001_initial.py
+++ mailchimp/migrations/0001_initial.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
+        ('contenttypes', '0001_initial'),
     ]
 
     operations = [
@@ -29,6 +29,7 @@ class Migration(migrations.Migration):
                 'verbose_name_plural': 'Mailchimp Logs',
                 'permissions': [('can_view', 'Can view Mailchimp information'), ('can_send', 'Can send Mailchimp newsletters')],
             },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Queue',
@@ -39,9 +40,9 @@ class Migration(migrations.Migration):
                 ('list_id', models.CharField(max_length=50)),
                 ('template_id', models.PositiveIntegerField()),
                 ('subject', models.CharField(max_length=255)),
-                ('from_email', models.EmailField(max_length=254)),
+                ('from_email', models.EmailField(max_length=75)),
                 ('from_name', models.CharField(max_length=255)),
-                ('to_email', models.EmailField(max_length=254)),
+                ('to_email', models.EmailField(max_length=75)),
                 ('folder_id', models.CharField(max_length=50, null=True, blank=True)),
                 ('tracking_opens', models.BooleanField(default=True)),
                 ('tracking_html_clicks', models.BooleanField(default=True)),
@@ -61,13 +62,19 @@ class Migration(migrations.Migration):
                 ('locked', models.BooleanField(default=False)),
                 ('content_type', models.ForeignKey(blank=True, to='contenttypes.ContentType', null=True)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Reciever',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('email', models.EmailField(max_length=254)),
+                ('email', models.EmailField(max_length=75)),
                 ('campaign', models.ForeignKey(related_name='recievers', to='mailchimp.Campaign')),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
     ]
